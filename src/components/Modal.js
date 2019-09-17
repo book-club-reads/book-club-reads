136fe20b3d9bb17b6c2c1a369e5fbb66c7bdd610
Shirley Wong <shirleywsywong@gmail.com>
2019-09-17 18:12:56 -0400

    search result styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -25,36 +25,35 @@ class Modal extends React.Component {
               className="closeModalButton"
               onClick={this.props.close}
             >
-              &#9747;
+              {/* &#9747; */}
+              x
             </span>
           </div>
-          <div className="wrapper">
-            <div className="bookDisplay">
-              <div className="bookImage">
-                <img
-                  src={
-                    this.props.img ===
-                      "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png"
-                      ? placeholder
-                      : this.props.img
-                  }
-                  alt={this.props.title}
-                />
-              </div>
-              <div className="modalBody">
-                <h2>{this.props.title}</h2>
-                <p>Author: {this.props.author}</p>
-                <p>Rating: {this.props.rating}</p>
-                {this.state.addShowing &&
-                  <button
-                    onClick={() => { this.props.addBook(this.props.selectBook); this.buttonSwitch() }
-                    }
-                  >
-                    Add Book
-                  </button>
+          <div className="bookDisplay">
+            <div className="bookImage">
+              <img
+                src={
+                  this.props.img ===
+                    "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png"
+                    ? placeholder
+                    : this.props.img
                 }
-                {this.state.addedShowing && <button>Added</button>}
-              </div>
+                alt={this.props.title}
+              />
+            </div>
+            <div className="modalBody">
+              <h2>{this.props.title}</h2>
+              <p>Author: {this.props.author}</p>
+              <p>Rating: {this.props.rating}</p>
+              {this.state.addShowing &&
+                <button
+                  onClick={() => { this.props.addBook(this.props.selectBook); this.buttonSwitch() }
+                  }
+                >
+                  Add Book
+                </button>
+              }
+              {this.state.addedShowing && <button>Added</button>}
             </div>
           </div>
         </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -20,14 +20,15 @@ class Results extends Component {
   //Display the search results
   renderDisplayBooks = () => {
     const bookList = this.props.displayBookResults.map((book, i) => {
-      const first20 = book.best_book.title.replace(/(.{20})..+/, "$1...");
+      const first20 = book.best_book.title.replace(/(.{30})..+/, "$1...");
       return (
-        <div key={i} className="resultsBlock">
+        <div key={i} className="resultsBlock"
+          onClick={() => {
+            this.selectBook(book);
+          }}>
           <div
             className="bookImages"
-            onClick={() => {
-              this.selectBook(book);
-            }}
+            
           >
             <img
               src={
@@ -39,7 +40,7 @@ class Results extends Component {
               alt={book.best_book.title}
             />
           </div>
-          <p className="bookNameResults">{book.best_book.title}</p>
+          <p className="bookNameResults">{book.best_book.title.length < 30 ? book.best_book.title : first20}</p>
           <p className="authorNameResults">{book.best_book.author.name}</p>
           
         </div>
