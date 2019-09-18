f8df58e12443a1cde9a87def146b6933117e6d46
Gabe Kugelmass <gkord@me.com>
2019-09-17 23:04:01 -0400

    Updates

---
+++ precache-manifest.8e8b4fbb5912fadeee36388e3ffcd71e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129d1439c3063f15a254ee67b350ccac",
+    "url": "/book-club-reads/index.html"
+  },
+  {
+    "revision": "a7992f5a198fe19cdcb7",
+    "url": "/book-club-reads/static/css/main.f178adec.chunk.css"
+  },
+  {
+    "revision": "d16aebf9e13c02a40da9",
+    "url": "/book-club-reads/static/js/2.716f8d03.chunk.js"
+  },
+  {
+    "revision": "a7992f5a198fe19cdcb7",
+    "url": "/book-club-reads/static/js/main.12322f61.chunk.js"
+  },
+  {
+    "revision": "ffe8f78b6441fa6de75a",
+    "url": "/book-club-reads/static/js/runtime~main.21484ea7.js"
+  },
+  {
+    "revision": "6284bfd2c59c92581ff92057c724b759",
+    "url": "/book-club-reads/static/media/bookLogo.6284bfd2.png"
+  },
+  {
+    "revision": "1f176ca0c9e025f062e057399de95d7b",
+    "url": "/book-club-reads/static/media/header.1f176ca0.jpg"
+  }
+]);
