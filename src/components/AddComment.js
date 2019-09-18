c9b73aeb98858500df6069a5fd0618a11309c580
Gabe Kugelmass <gkord@me.com>
2019-09-17 20:49:22 -0400

    styled booklist page and modal

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,76 +1,87 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import firebase from "firebase";
 
 class AddComment extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            isShowing: false
-        };
-    }
-    
-    openModal = (books) => {
-        this.setState({
-            isShowing: true
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      isShowing: false
     };
+  }
 
-    closeModal = () => {
-        this.setState({
-            isShowing: false
-        })
-    }
-    handleChange = (event) => {
-        this.setState({ 
-            value: event.target.value 
-        });
-        this.openModal();
-    }
+  openModal = books => {
+    this.setState({
+      isShowing: true
+    });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        
-        const bookId = this.props.comment
-        const dbRef = firebase.database().ref("Name").child(`${bookId}`)
+  closeModal = () => {
+    this.setState({
+      isShowing: false
+    });
+  };
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+    this.openModal();
+  };
 
-        dbRef.update({
-            Comment: this.state.value
-        })
-        this.setState({
-            value: ''
-        })
-        this.closeModal();
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const bookId = this.props.comment;
+    const dbRef = firebase
+      .database()
+      .ref("Name")
+      .child(`${bookId}`);
+
+    dbRef.update({
+      Comment: this.state.value
+    });
+    this.setState({
+      value: ""
+    });
+    this.closeModal();
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.comment && this.props.comment !== prevProps.comment) {
+      this.openModal();
     }
-    
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.comment && this.props.comment !== prevProps.comment) {
-            this.openModal();
-        }
-    }
-    
-    render() {
-        return (
-            <div>
-            { this.state.isShowing && (
-            <div className="formModalContainer">
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isShowing && (
+          <div className="modal">
+              <div className="formModalContainer">
                 <div className="modalHeader">
-                    <span className="closeModalButton" onClick={this.closeModal}>
-                        &#9747;
-        </span>
+                  <span className="closeModalButton" onClick={this.closeModal}>
+                    x
+                  </span>
                 </div>
                 <div className="wrapper">
-                        <form className="formFlex" onSubmit={this.handleSubmit}>
-                            <label>Add Comment</label>
-            <textarea value={this.state.value} onChange={this.handleChange} rows={10} cols={5} placeholder="Add comments to the book" />
-                            <input type="submit" value="Post Comment" />
-                        </form>
+                  <form className="formFlex" onSubmit={this.handleSubmit}>
+                    <label className="commentLabel">Add Comment</label>
+                    <textarea
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                      rows={10}
+                      cols={5}
+                      placeholder="Add comments to the book"
+                    />
+                    <input className="commentSubmit" type="submit" value="Post Comment" />
+                  </form>
                 </div>
-            </div> 
-            )}
-            </div>
-        );
-    }
+              </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default AddComment;
